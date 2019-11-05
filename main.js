971791d2792fcc54d54a4977c79198ce70fecad5
Ilya Kiyanov <ikiyanov@protonmail.com>
2019-11-05 20:36:56 +0300

    adds first markup and js objects

---
+++ main.js
@@ -0,0 +1,12 @@
+var feedbacks = [];
+
+var feedback = {
+    id: undefined,
+    question_1: undefined,
+    question_2: undefined,
+    question_3: [],
+    question_4: [],
+    question_5: [],
+    question_6: undefined,
+    question_7: undefined
+}
