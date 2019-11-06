232ef4215232d8c0aea07c1a12c154db242f602c
elil_ <ikiyanov@protonmail.com>
2019-11-06 17:01:27 +0300

    some code for first question

--- main.js
+++ main.js
@@ -10,3 +10,20 @@ var feedback = {
     question_6: undefined,
     question_7: undefined
 }
+
+var pages = document.querySelectorAll('.page');
+var pageId = 0;
+
+//home page
+var btnStart = document.querySelector('#btn_start');
+
+btnStart.onclick = function() {
+    pages[pageId].classList.toggle('page--active');
+    pages[pageId + 1].classList.toggle('page--active');
+}
+
+//question 1
+function wordCount() {
+    return document.querySelector('#q1_value').value.length;
+}
+console.log('words: ' + wordCount());
