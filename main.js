1c3d797d2f2e82f238c9631c57cd8c1a48598537
elil_ <ikiyanov@protonmail.com>
2019-11-07 01:39:29 +0300

    fnish steps for markup

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
+//global
 var feedbacks = [];
 
 var feedback = {
-    id: undefined,
-    question_1: undefined,
-    question_2: undefined,
+    id: null,
+    question_1: null,
+    question_2: null,
     question_3: [],
     question_4: [],
     question_5: [],
-    question_6: undefined,
-    question_7: undefined
+    question_6: null,
+    question_7: null
 }
 
 var pages = document.querySelectorAll('.page');
 var pageId = 0;
+
+var questions = document.querySelectorAll('.question');
+var qstnId = 0;
+
+var formQuest = document.forms.questionnaire;
+
+function radioValue(radios) {
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            return radios[i].value;
+        }
+    }
+}
 
 //home page
 var btnStart = document.querySelector('#btn_start');
@@ -20,17 +34,37 @@ var btnStart = document.querySelector('#btn_start');
 btnStart.onclick = function() {
     pages[pageId].classList.toggle('page--active');
     pages[pageId + 1].classList.toggle('page--active');
+
+    questions[qstnId].classList.toggle('question--active');
 }
 
 //question 1
-var formQ1 = document.forms.question1;
-var userNameInput = formQ1.elements.user_name;
+var userNameInput = formQuest.elements.user_name;
+var submitQ1 = document.querySelector('#submitQ1');
 
 userNameInput.oninput = function() {
     if (userNameInput.value.length >= 3) {
-        document.querySelector('#submitQ1').removeAttribute('disabled');
+        submitQ1.removeAttribute('disabled');
     }
 }
+
+submitQ1.onclick = function() {
+    feedback.question_1 = userNameInput.value;
+
+    questions[qstnId].classList.toggle('question--active');
+    questions[qstnId + 1].classList.toggle('question--active');
+}
+
+//question 2
+document.querySelector('#submitQ2').onclick = function() {
+    var radios = document.querySelectorAll('fieldset[name="question2"] input[type="radio"]');
+    feedback.question_2 = radioValue(radios);
+}
+
+//question6
+flatpickr("#q6_calendar", {
+    minDate: "today"
+});
 
 //dev elements
 var testBtn = document.querySelector('#btn_test');
